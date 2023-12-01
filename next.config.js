fcf19b2e35e36eb1b378da63563f173e0227005e
psylum <perry.rajnovic@gmail.com>
2023-12-01 18:33:21 -0500

    Enable transpiling of both libraries
    Enabling transpilation of astrouxds is required when using the library
    directly. When using the library that references it, that library must
    also be forcibly included in the transpile list for it to work.

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  transpilePackages: ['@org/acl-poc-lib', '@astrouxds/react'],
 }
 
 module.exports = nextConfig
